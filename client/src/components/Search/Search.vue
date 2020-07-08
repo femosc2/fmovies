8b31a45e9b7bc2aef339b4bc0ce84b1348b7df41
Felix Morau <felixmorau@gmail.com>
2020-07-08 21:19:15 +0200

    change placeholder text, optimize searchabr code

--- client/src/components/Search/Search.vue
+++ client/src/components/Search/Search.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <input type="text" v-model="search" v-on:keyup="setFilter" autofocus placeholder="Not Yet Implemented" />
+    <input type="text" v-model="search" v-on:keyup="setFilter" autofocus placeholder="Search for a movie, parts of the plot or release year" />
   </div>
 </template>
 
@@ -16,13 +16,16 @@ export default {
   computed: {
     movies() {
       return this.$store.state.movies;
+    },
+    query() {
+      return this.search.toLowerCase()
     }
   },
   methods: {
     setFilter() {
         this.$store.commit('setSearch', this.search.toLowerCase())
         this.filter = this.movies.filter((m) => 
-            m.Title.toLowerCase().includes(this.search.toLowerCase()) || m.Director.toLowerCase().includes(this.search.toLowerCase()) || m.Plot.toLowerCase().includes(this.search.toLowerCase()) || m.Year.toLowerCase().includes(this.search.toLowerCase())
+            m.Title.toLowerCase().includes(this.query) || m.Director.toLowerCase().includes(this.query) || m.Plot.toLowerCase().includes(this.query) || m.Year.toLowerCase().includes(this.query)
         )
         this.$store.commit('setFilteredMovies', this.filter);
     }
