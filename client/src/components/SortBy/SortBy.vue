2b8179477552f733cd5d7a828eab7ba848db512f
femosc2 <felixmorau@gmail.com>
2020-11-13 21:17:35 +0100

    add css for sortby buttons

--- client/src/components/SortBy/SortBy.vue
+++ client/src/components/SortBy/SortBy.vue
@@ -1,7 +1,8 @@
 <template>
 <div>
-  <h2>Sort By:</h2>
+  <h2>Sort By</h2>
   <button @click="setSortBy('rating')">Rating</button>
+  <br />
   <button @click="setSortBy('watched')">Recently Watched</button>
 </div>
 </template>
@@ -18,13 +19,36 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="css" scoped>
+<style lang="scss" scoped>
 
 div {
   position: fixed;
   top: 0;
+  right: 0;
+  background-color: none;
+  height: 250px;
+  width: 250px;
+  color: #fff;
+  opacity: 0.25;
+  transition: 0.25s;
+  &:hover {
+    opacity: 1;
+    transition: 0.25s;
+  }
+}
+
+button {
+  border: 0;
+  background: none;
+  border: 1px solid #2c3e50;
+  font-size: 25px;
+  color: #000;
+  background-color: rgba(255,255,255, 0.80); 
+  height: 150px;
+  width: 150px;
+  text-align: center;
   left: 0;
-  background-color: white;  
+  border-radius: 50%;
 }
 
 @media screen and (min-width: 700px) {
