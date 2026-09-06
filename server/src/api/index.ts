dcd140cb0e1803d0e295a8866fb66474db3d1852
Felix Morau <felixmorau@gmail.com>
2026-09-06 16:04:32 +0200

    chore: remove legacy Express server
    The Express server is fully replaced by the Vercel serverless functions in api/.

--- server/src/api/index.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import movieRouter from './routes/movieRoute';
-const api = express();
-
-api.use('/movie', movieRouter)
-
-export default api;
--- server/src/api/routes/movieRoute.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { createMovie, createMovieById } from '../controllers/movieController';
-const movieRouter = Router();
-
-movieRouter.post('/', createMovie);
-movieRouter.post('/id', createMovieById);
-
-export default movieRouter;
